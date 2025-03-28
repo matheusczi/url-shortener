b63ff2bf6a0c208ed979df883be34dd34e3bd041
matheusczi <matheusczi@hotmail.com>
2025-03-28 17:35:03 -0300

    feat: add url module and controller

--- backend/src/url/url.controller.ts
+++ backend/src/url/url.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Body,
+  Res,
+  NotFoundException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { UrlService } from './url.service';
 
 @Controller('url')
-export class UrlController {}
+export class UrlController {
+  constructor(private readonly urlService: UrlService) {}
+
+  @Post('shorten')
+  async shorten(@Body('url') url: string) {
+    return this.urlService.shortenUrl(url);
+  }
+
+  @Get(':slug')
+  async redirect(@Param('slug') slug: string, @Res() res: Response) {
+    const url = await this.urlService.findBySlug(slug);
+    if (!url) throw new NotFoundException('URL not found');
+    res.redirect(url.originalUrl);
+  }
+}
