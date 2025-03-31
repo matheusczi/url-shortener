ec6c29fff7d73f86999a21f27d9adb401c073d0e
matheusczi <matheusczi@hotmail.com>
2025-03-31 15:24:19 -0300

    feat: register validator in URL module

--- backend/src/url/url.module.ts
+++ backend/src/url/url.module.ts
@@ -3,10 +3,11 @@ import { UrlService } from './url.service';
 import { UrlController } from './url.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Url } from './url.entity';
+import { UniqueSlugValidator } from './validators/unique-slug.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Url])],
-  providers: [UrlService],
+  providers: [UrlService, UniqueSlugValidator],
   controllers: [UrlController],
   exports: [UrlService],
 })
