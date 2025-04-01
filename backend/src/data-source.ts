e1d4b531128da16419f9ab70030c6c7d2c963cb3
matheusczi <matheusczi@hotmail.com>
2025-04-01 14:01:41 -0300

    chore: add main .env to postgres setup

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -6,7 +6,7 @@ const AppDataSource = new DataSource({
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'root',
+  password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'url_shortener',
   entities: [__dirname + '/**/*.entity.{ts,js}'],
   migrations: [__dirname + '/migrations/*.ts'],
